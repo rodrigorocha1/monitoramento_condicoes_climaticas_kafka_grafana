11a12c36abd859f004f7a626248de839f2cbf61b
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-02-13 22:12:29 -0300

    correção criação de pasta

--- recriar_pastas.sh
+++ recriar_pastas.sh
@@ -2,8 +2,6 @@
 
 sudo rm -rf influxdb2-data/ influxdb2-config/ grafana_data/
 mkdir -p influxdb2-data/ influxdb2-config/ grafana_data/
-sudo chmod -R 777 influxdb2-data/ influxdb2-config/
-sudo chmod -R 775 grafana_data/
+sudo chmod -R 777 influxdb2-data/ influxdb2-config/ grafana_data/
 sudo chown -R 472:472 influxdb2-data/ influxdb2-config/
-
 echo "Diretórios configurados com sucesso."
